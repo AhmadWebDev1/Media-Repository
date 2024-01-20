557cb73de7723dfd985a4e5aea2f531e4968091b
AhmadWebDev <155267147+AhmadWebDev1@users.noreply.github.com>
2024-01-20 19:57:28 +0100

    Update LodynetProvider.kt

--- LodynetProvider/src/main/kotlin/com/lodynet/LodynetProvider.kt
+++ LodynetProvider/src/main/kotlin/com/lodynet/LodynetProvider.kt
@@ -1,14 +1,12 @@
 package com.lodynet
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.Qualities
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.Document
-import java.util.regex.Pattern
-import java.util.regex.Matcher
 
 class Lodynet : MainAPI() {
     override var lang = "ar"
@@ -37,64 +35,106 @@ class Lodynet : MainAPI() {
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val doc = app.get(request.data + page).document
-        val list = doc.select(".BlocksArea li").mapNotNull { element ->
-            element.toSearchResponse()
+        val duplicateTitles = mutableSetOf<String>()
+        val list = doc.select(".BlocksArea li").mapNotNull {
+            it.toSearchResponse(duplicateTitles)
         }
         return newHomePageResponse(request.name, list)
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
+        val searchResults = ArrayList<SearchResponse>()
         val q = query.replace(" ", "+")
-        return app.get("$mainUrl/search/$q").document.select(".BlocksArea li").mapNotNull {
-            it.toSearchResponse()
-        }
-    }
-
-    override suspend fun load(url: String): LoadResponse {
-        val episodes = ArrayList<Episode>()
-        val doc = app.get(url).document
-        val posterUrl = doc.select(".Poster img").attr("src")
-        val title = extractTitle(doc)
-        val synopsis = doc.select(".DetailsBoxContentInner").text()
-        val isMovie = doc.select(".category").text().contains("افلام")
-
-        val currentPageEpisodes = extractEpisodes(doc, title)
-        episodes.addAll(currentPageEpisodes)
+        val duplicateTitles = mutableSetOf<String>()
 
         var currentPage = 1
         var hasNextPage = true
-
         while (hasNextPage) {
-            val nextPageUrl = "$url/page/$currentPage"
-            val nextPageDoc = app.get(nextPageUrl).document
+            val nextPageDoc = app.get("$mainUrl/search/$q/page/$currentPage").document
+            val nextPagePosts = nextPageDoc.select(".BlocksArea li").mapNotNull {
+                it.toSearchResponse(duplicateTitles)
+            }
+            searchResults.addAll(nextPagePosts)
 
-            val nextPageEpisodes = extractEpisodes(nextPageDoc, title)
-            episodes.addAll(nextPageEpisodes)
-
-            val nextPageElement = nextPageDoc.select(".pagination .next")
+            val nextPageElement = nextPageDoc.select(".pagination li:has(a:contains(التالية))")
             hasNextPage = nextPageElement.isNotEmpty()
 
             currentPage++
+        }
+
+        return searchResults
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val doc = app.get(url).document
+
+        if (doc.select(".MainSingleCover").isNotEmpty()) {
+            return loadPage(doc, url)
+        } else {
+            val postUrl = doc.select(".BlocksArea li a").attr("href")
+            val docu2 = app.get(postUrl).document
+            return loadPage(docu2, postUrl)
+        }
+    }
+
+    private suspend fun loadPage(doc: Document, url: String): LoadResponse {
+        val isMovie = doc.select(".category").text().contains("افلام")
+        val posterUrl = doc.select(".Poster img").attr("src")
+        val title = cleanTitle(doc.select(".TitleSingle h1").text().replace("((حلقة|الحلقة)\\s*(\\d+))|(والاخيرة|والأخيرة|الاخيرة|الأخيرة)".toRegex(), "").trim())
+        val synopsis = doc.select(".DetailsBoxContentInner").text()
+        val tags = doc.select(".TitleSingle .genre").map { it.text() }
+        val actors = doc.select(".Actors li")?.mapNotNull {
+            val name = it.select("a")?.text().toString()
+            val image = it.select("a img")?.attr("src")
+            Actor(name, image)
+        }
+        val duplicateTitles = mutableSetOf<String>()
+        val recommendations = doc.selectFirst(".RelatedSection")?.select(".BlocksArea li")?.mapNotNull {
+            it.toSearchResponse(duplicateTitles)
+        }
+
+        val episodes = ArrayList<Episode>()
+        doc.select(".EpisodesList a").forEach { el ->
+            episodes.add(
+                Episode(
+                    el.attr("href"),
+                    cleanTitle(el.attr("title").replace(title, "")),
+                    null,
+                    null
+                )
+            )
         }
 
         return if (isMovie) {
             newMovieLoadResponse(title, url, TvType.Movie, url) {
                 this.posterUrl = posterUrl
                 this.plot = synopsis
+                this.tags = tags
+                this.recommendations = recommendations
+                addActors(actors)
             }
         } else {
             newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes.distinct().reversed()) {
                 this.posterUrl = posterUrl
+                this.plot = synopsis
+                this.tags = tags
+                this.recommendations = recommendations
+                addActors(actors)
             }
         }
     }
 
-    private fun Element.toSearchResponse(): SearchResponse? {
+    private fun Element.toSearchResponse(duplicateTitles: MutableSet<String>): SearchResponse? {
         val url = select("a").attr("href")
-        val title = select(".SliderItemDescription h2").text()
+        val title = cleanTitle(select(".SliderItemDescription h2").text().replace("((حلقة|الحلقة)\\s*(\\d+))|(والاخيرة|والأخيرة|الاخيرة|الأخيرة)".toRegex(), "").trim())
         val posterUrl = select("img").attr("src")
+
+        if (duplicateTitles.contains(title)) {
+            return null
+        }
+        duplicateTitles.add(title)
         return MovieSearchResponse(
-            cleanTitle(title),
+            title,
             url,
             name,
             null,
@@ -104,41 +144,6 @@ class Lodynet : MainAPI() {
         )
     }
 
-    private fun cleanTitle(title: String): String {
-        return title.replace("فيلم|مترجم|مسلسل|مشاهدة".toRegex(), "")
-        .replace("التايلاندي|الصيني|عربي|للعربي|الكوري|حصرياً".toRegex(), "")
-        .replace("الأكشن|والرعب|الرومانسية|والميلودراما||والدراما|والخيال|والإثارة|الإثارة|المغامرة|والمغامرة|والخيال العلمي|الانيميشن|و الفانتازيا".toRegex(), "")
-        .trim()
-    }
-
-    private fun extractEpisodes(doc: Document, title: String): List<Episode> {
-        val episodes = ArrayList<Episode>()
-        
-        doc.select(".BlocksArea li>a").forEach { el ->
-            episodes.add(
-                Episode(
-                    el.attr("href"),
-                    cleanTitle(el.select(".SliderItemDescription h2").text().replace(title, "")),
-                    null,
-                    posterUrl = el.select("img").attr("src")
-                )
-            )
-        }
-        
-        return episodes
-    }
-
-    private fun extractTitle(doc: Document): String {
-        val titleElement = doc.select(".TitleSingle > ul > li:nth-child(1) > a")
-        return if (doc.select(".TitleSingle h1").text().contains("فيلم")) {
-            cleanTitle(doc.select(".TitleSingle h1").text().replace("مترجم", ""))
-        } else if (titleElement.isNotEmpty()) {
-            cleanTitle(titleElement.text())
-        } else {
-            cleanTitle(doc.select(".TitleInner h2").text())
-        }
-    }
-
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
@@ -146,40 +151,44 @@ class Lodynet : MainAPI() {
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         val doc = app.get(data).document
-        val iframeNameMap = mapOf(
-            "uqload.io" to "Uqload",
-            "wishfast.top" to "Playersb",
-            "govad.xyz" to "Govid",
-            "vadbam.net" to "Bom-ser",
-            "vidlo.us" to "ViD LO",
-            "viidshar.com" to "Vid Tv"
-        )
+        
         doc.select("ul.ServersList > li").apmap {
             val iframeUrl = it.attr("data-embed")
-            val iframeDomain = iframeNameMap.keys.firstOrNull { iframeUrl.contains(it) }
+            val iframeName = it.text().toString()
 
-            if (iframeUrl.contains(iframeDomain.toString())) {
+            if (iframeUrl.contains("ok.ru")) {
+                loadExtractor(iframeUrl.replace("ok.ru/video/", "ok.ru/videoembed/"), data, subtitleCallback, callback)
+            } else {
                 val iframeDoc = app.get(iframeUrl).document
-                val iframeName = iframeNameMap[iframeDomain]
+                val matchResult = Regex("sources:\\s*\\[\\{[^}]*?file:\"(.*?)\"").find(iframeDoc.html()) ?: Regex("sources:\\s*\\[\"(https://.*?)\"]").find(iframeDoc.html())
+                val matchResult2 = Regex("sources:\\s*\\[\\{[^}]*?label:\"(.*?)\"").find(iframeDoc.html())
 
-                val regexPattern = Pattern.compile("sources:\\s*\\[\\{[^}]*?file:\"(.*?)\"", Pattern.DOTALL)
-                val matcher = regexPattern.matcher(iframeDoc.html())
-                while (matcher.find()) {
-                    val videoUrl = matcher.group(1)
+                if (matchResult != null) {
+                    val videoUrl = matchResult?.groups?.get(1)?.value.toString() 
+                    val quality = matchResult2?.groups?.get(1)?.value?.replace(Regex("[^0-9]"), "")?.toInt() ?: Qualities.Unknown.value
                     callback.invoke(
                         ExtractorLink(
                             this.name,
-                            iframeName.toString(),
+                            iframeName,
                             videoUrl,
-                            this.mainUrl,
-                            Qualities.Unknown.value,
+                            videoUrl,
+                            quality,
+                            videoUrl.contains("m3u8"),
                         )
                     )
+                } else {
+                    null
                 }
-            } else {
-                loadExtractor(iframeUrl.replace("ok.ru/video/", "ok.ru/videoembed/"), data, subtitleCallback, callback)
             }
         }
         return true
+    }
+
+    private fun cleanTitle(title: String): String {
+        return title.replace("فيلم|مترجم|مسلسل|مشاهدة".toRegex(), "")
+        .replace("التايلاندي|الصيني|عربي|للعربي|الكوري|حصرياً".toRegex(), "")
+        .trim()
+        // .replace("الأكشن|والرعب|الرومانسية|والميلودراما||والدراما|والخيال|والإثارة|الإثارة|المغامرة|والمغامرة|والخيال العلمي|الانيميشن|و الفانتازيا".toRegex(), "")
+        // .trim()
     }
 }
