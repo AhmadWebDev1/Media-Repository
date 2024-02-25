e170d7c7d3458ec0202f7426f8349110e19b12a1
AhmadWebDev1 <155267147+AhmadWebDev1@users.noreply.github.com>
2024-02-25 15:20:32 +0100

    Add files via upload

--- src/ar/aresmangascarlet/src/eu/kanade/tachiyomi/extension/ar/aresmangascarlet/AresMangaScarlet.kt
+++ src/ar/aresmangascarlet/src/eu/kanade/tachiyomi/extension/ar/aresmangascarlet/AresMangaScarlet.kt
@@ -1,7 +1,13 @@
 package eu.kanade.tachiyomi.extension.ar.aresmangascarlet
 
+import android.util.Base64
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.Page
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
+import org.jsoup.nodes.Document
+import java.lang.IllegalArgumentException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -23,5 +29,23 @@ class AresMangaScarlet : MangaThemesia(
         this.contains("Ongoing", ignoreCase = true) -> SManga.ONGOING
         this.contains("Completed", ignoreCase = true) -> SManga.COMPLETED
         else -> SManga.UNKNOWN
+    }
+
+    // =============================== Pages ================================
+    override fun pageListParse(document: Document): List<Page> {
+        // "ts_reader.run({" in base64
+        val script = document.selectFirst("script[src^=data:text/javascript;base64,dHNfcmVhZGVyLnJ1bih7]")
+            ?: return super.pageListParse(document)
+        val data = Base64.decode(script.attr("src").substringAfter("base64,"), Base64.DEFAULT).toString(Charsets.UTF_8)
+        val imageListJson = JSON_IMAGE_LIST_REGEX.find(data)?.destructured?.toList()?.get(0).orEmpty()
+        val imageList = try {
+            json.parseToJsonElement(imageListJson).jsonArray
+        } catch (_: IllegalArgumentException) {
+            emptyList()
+        }
+
+        return imageList.mapIndexed { i, jsonEl ->
+            Page(i, imageUrl = jsonEl.jsonPrimitive.content)
+        }
     }
 }
