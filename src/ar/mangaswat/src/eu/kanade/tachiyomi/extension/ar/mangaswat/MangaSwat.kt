43893e1877e82d3603e92ec152291475b6b49219
AhmadWebDev1 <155267147+AhmadWebDev1@users.noreply.github.com>
2024-02-25 18:31:22 +0100

    Update MangaSwat.kt

--- src/ar/mangaswat/src/eu/kanade/tachiyomi/extension/ar/mangaswat/MangaSwat.kt
+++ src/ar/mangaswat/src/eu/kanade/tachiyomi/extension/ar/mangaswat/MangaSwat.kt
@@ -1,8 +1,10 @@
 package eu.kanade.tachiyomi.extension.ar.mangaswat
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.Request
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -11,13 +13,31 @@ class MangaSwat : MangaThemesia(
     "MangaSwat",
     "https://swatmanhua.com",
     "ar",
+    "/manga",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar")),
 ) {
+    // =============================== Search ===============================
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val request = super.searchMangaRequest(page, query, filters)
+        if (query.isBlank()) return request
+
+        val url = request.url.newBuilder()
+            .removePathSegment(0)
+            .removeAllQueryParameters("title")
+            .addQueryParameter("s", query)
+            .build()
+
+        return request.newBuilder()
+            .url(url)
+            .build()
+    }
+
     // =========================== Manga Details ============================
-    override val seriesArtistSelector = ".tsinfo .imptdt:contains(الرسام) i"
-    override val seriesAuthorSelector = ".tsinfo .imptdt:contains(المؤلف) i"
-    override val seriesTypeSelector = ".tsinfo .imptdt:contains(النوع) i"
-    override val seriesStatusSelector = ".tsinfo .imptdt:contains(الحالة) i"
+    override val seriesArtistSelector = ".infox span:contains(الرسام) i, ${super.seriesArtistSelector}"
+    override val seriesAuthorSelector = ".infox span:contains(المؤلف) i, ${super.seriesAuthorSelector}"
+    override val seriesGenreSelector = ".infox span:contains(التصنيف) a, ${super.seriesGenreSelector}"
+    override val seriesTypeSelector = ".infox span:contains(النوع) a, ${super.seriesTypeSelector}"
+    override val seriesStatusSelector = ".infox span:contains(الحالة) a, ${super.seriesStatusSelector}"
 
     override fun String?.parseStatus() = when {
         this == null -> SManga.UNKNOWN
@@ -27,6 +47,7 @@ class MangaSwat : MangaThemesia(
         else -> SManga.UNKNOWN
     }
 
+    // ============================ Chapter list ============================
     override fun chapterFromElement(element: Element) = SChapter.create().apply {
         val urlElements = element.select("a")
         setUrlWithoutDomain(urlElements.attr("href"))
