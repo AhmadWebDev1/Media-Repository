85de3709066db423309734e738c966a03aed944e
AhmadWebDev <155267147+AhmadWebDev1@users.noreply.github.com>
2025-03-06 18:08:57 +0100

    Update build.gradle.kts

--- ArabicToonsProvider/build.gradle.kts
+++ ArabicToonsProvider/build.gradle.kts
@@ -5,6 +5,6 @@ cloudstream {
     authors = listOf( "Ahmad" )
     language = "ar"
     status = 1
-    tvTypes = listOf( "Anime" , "AnimeMovie", "Live" )
+    tvTypes = listOf("Anime", "AnimeMovie", "Live")
     iconUrl = "https://www.google.com/s2/favicons?sz=%size%&www.arabic-toons.com"
 }
