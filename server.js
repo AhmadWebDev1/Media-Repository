2e5c714bb11a32fd5b2e5ed018a564f76ab4c6af
AhmadWebDev <155267147+AhmadWebDev1@users.noreply.github.com>
2025-12-08 22:38:04 +0100

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const addonInterface = require("./addon");
-
-serveHTTP(addonInterface, { port: process.env.PORT || 3000 });
-
-console.log("Addon running on: http://localhost:3000/manifest.json");
